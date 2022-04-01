22a86e8c3b72d43cec32c209cc2d5c093ef12527
Carla <carladmaximo@gmail.com>
2022-04-01 18:06:52 -0300

    alteracoes main

--- src/components/EstadoAtual.tsx
+++ src/components/EstadoAtual.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const EstadoAtual:React.FC = () => {
   return (
-    <div className="my-3 mx-auto my-4">
-      Estado atual
+    <div className="my-3 mx-auto my-4 d-flex">
+      <h4>Cabeça:</h4>
+      <h4>Braço Esquerdo:</h4>
+      <h4>Braço Direito:</h4>
     </div>
   );
 }
